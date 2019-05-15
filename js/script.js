0905bc9caac0ee5d2ea08a769aa1532911385068
ranjoshua <46650701+ranjoshua@users.noreply.github.com>
2019-05-15 15:57:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ let toggleNav = function(){
 
 
 let backgroundImageSize = function(){
+	document.body.style.width = window.innerWidth + "px";
 	var size = windowInnerHeight - mainbarHeight- 81 + "px";
 	document.getElementsByClassName("title")[0].setAttribute("style","height:" + size);
 }();
